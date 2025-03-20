ff04126fb5d9e911ef671d843cc73efe94fac90b
karlsjoe <joel.e.karlsson2@gmail.com>
2025-03-20 15:17:15 +0200

    Code challenge 1

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,6 +1,7 @@
 package fi.arcada.codechallenge;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        TextView  test = (TextView) findViewById(R.id.hello);
+test.setText("Min app funkar!");
     }
 }
